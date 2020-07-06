b1b5ad08e4493ae5d63e40f54bf898ca00719ab6
Jessica Benson <pokeyjess72@gmail.com>
2020-07-06 10:58:26 -0400

    prints images in alphabetical

--- logpuzzle.py
+++ logpuzzle.py
@@ -14,20 +14,32 @@ HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US;
 rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 """
 
+__author__ = 'pokeyjess'
+
+import argparse
 import os
 import re
 import sys
 import urllib.request
-import argparse
 
 
 def read_urls(filename):
-    """Returns a list of the puzzle URLs from the given log file,
-    extracting the hostname from the filename itself, sorting
-    alphabetically in increasing order, and screening out duplicates.
-    """
-    # +++your code here+++
-    pass
+    """Returns a list of the puzzle urls from the given log file,
+    extracting the hostname from the filename itself.
+    Screens out duplicate urls and returns the urls sorted into
+    increasing order."""
+
+    domain = "http://" + filename.split("_")[1]
+
+    urls = set()
+    photos = re.findall(r'GET \/(.*?\.jpg)', open(filename).read())
+    photos = re.findall(r'GET (\/.*?\.jpg)', open(filename).read())
+
+    for photo in photos:
+        urls.add(domain + photo)
+    return sorted(urls)
+
+# remove duplicates, regular expression
 
 
 def download_images(img_urls, dest_dir):
